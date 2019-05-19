7b57bf56892f269c372e852d83e7507e6a5ae4e8
Marcus <marcusvcsilva@gmail.com>
2019-05-19 17:38:27 -0300

    triggering deploy

--- app.js
+++ app.js
@@ -9,14 +9,9 @@ mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/msgdb', {useNewUrlParser: true});
 
-app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 routes(app);
-
-/*app.get('/url', (req, res, next) => {
- console.log((new Date()).getTime());
- res.json({'headers': req.headers, 'route': req.route});
-});*/
 
 app.listen(port, function() {
     console.log('Server running on %s', port);
